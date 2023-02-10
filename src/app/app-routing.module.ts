c6b9f7254516f723c2e32d8780fef38c0259d84d
esraa1711 <esraakhaled163@gmail.com>
2023-02-10 15:01:13 +0200

    RegisterUpdate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -9,8 +11,16 @@ const routes: Routes = [
     component: ProductListComponent
   },
   {
+    path:"login",
+    component: LoginComponent
+  },
+  {
     path:"product details/:id",
     component: ProductDetailsComponent
+  },
+  {
+    path:"register",
+    component: RegisterComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { ProductCardComponent } from './product-card/product-card.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { CurrencyPipe } from './pipes/currency.pipe';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { LoginComponent } from './login/login.component';
+import {FormsModule} from "@angular/forms";
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
@@ -18,11 +24,15 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductCardComponent,
     AddProductComponent,
     CurrencyPipe,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
